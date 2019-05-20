bde7b9cfcb2180c80b7ec60896121908290278a8
kimwoongkyu <darkgantz@naver.com>
2019-05-20 17:24:05 +0900

    remove override

--- android/src/main/java/org/capslock/RNDeviceBrightness/RNDeviceBrightness.java
+++ android/src/main/java/org/capslock/RNDeviceBrightness/RNDeviceBrightness.java
@@ -16,7 +16,6 @@ import java.util.List;
 
 public class RNDeviceBrightness implements ReactPackage {
 
-  @Override
   public List<Class<? extends JavaScriptModule>> createJSModules() {
     return Collections.emptyList();
   }
